54769e5ac2a3279578e92f0c51227181173ea664
andre <andrepereiraleite@gmail.com>
2015-02-04 17:48:55 -0300

    imagens

--- courses/mod.py
+++ courses/mod.py
@@ -1,29 +1,41 @@
-from simplemooc.courses.models import Course
+#executar no shell do django
+
+from courses.models import Course
 
 django = Course(name='Python com Django', slug='django')
 django.save()
-django.pk
+# django.pk
 
 python_dev = Course(name='Python para alguma coisa', slug='python-dev')
 python_dev.save()
-python_dev.pk
+# python_dev.pk
 
 courses = Course.objects.all()
 for course in courses:
     print(course.name)
 
-courses[0].name
+# courses[0].name
 courses = Course.objects.filter(slug='django')
-courses
+# courses
 
-courses[0]
+# courses[0]
 courses = Course.objects.filter(slug='django').filter(name='Python')
-courses
+# courses
 courses = Course.objects.filter(slug='django', name='Python')
-courses
+# courses
 courses = Course.objects.filter(name__icontains='Python')
 type('Course.db.models.manager.manager')
 type(courses)
-Course.objects.get_queryset()
-courses.delete()
+# Course.objects.get_queryset()
+# courses.delete()
 
+
+#parte2
+
+from courses.models import Course
+
+Course.objects.create(name='Python com django', slug='django')
+Course.objects.create(name='Python para devs', slug='python')
+Course.objects.create(name='Python para logica', slug='logica')
+
+Course.objects.search('python')
--- courses/models.py
+++ courses/models.py
@@ -3,7 +3,10 @@ from django.db import models
 
 class CourseManager(models.Manager):
     def search(self, query):
-        return self.get_queryset().filter(name__icontains=query)
+        # return self.get_queryset().filter(name__icontains=query, description__icontains=query)#&&
+        return self.get_queryset().filter(
+            models.Q(name__icontains=query) | models.Q(description__icontains=query)
+        )#||
 
 
 class Course(models.Model):
@@ -19,3 +22,12 @@ class Course(models.Model):
     created_at = models.DateTimeField('Criado em', auto_now_add=True)
     updated_at = models.DateTimeField('Atualizado em', auto_now=True)
 
+    objects = CourseManager()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:#forma que os nomes serao exibidas
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+        ordering = ['name']#ordem que serao exibida
